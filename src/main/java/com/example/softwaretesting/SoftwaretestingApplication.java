aa871f7ab93f60ae354c74b18db798a1f3dec210
Sohan Jain <snsohanjain@gmail.com>
2023-03-09 17:58:30 +0530

    first commit

---
+++ src/main/java/com/example/softwaretesting/SoftwaretestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwaretesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwaretestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwaretestingApplication.class, args);
+	}
+
+}
